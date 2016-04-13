8a00454790c41b9a351cbcf79cef4a8974cfcbe1
Bertrand Caré <bertrand.care@gmail.com>
2016-04-13 21:32:02 +0200

    fixed bundling script on windows

--- roverclient/bundle-for-win32.ps1
+++ roverclient/bundle-for-win32.ps1
@@ -3,12 +3,35 @@ Add-Type -assembly System.IO.Compression.FileSystem
 $compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
 
 pyinstaller  --windowed rvclient-win32.spec
-
-cd dist
-   [System.IO.Compression.ZipFile]::CreateFromDirectory("rvclient-win32","..\bundles\rvclient-win32.zip", $compressionLevel, $false)
-
-
-cd ..
-
 pyinstaller --onefile --windowed rvclient-win32-standalone.spec
-cp dist\rvclient-win32-standalone.exe bundles\.
+
+$currentwd= (Convert-Path .)
+$currentdate = (Get-Date -format yyyyMMdd)
+$rvversion = (git describe)
+$suffix = "$rvversion-$currentdate"
+
+$distpath = (Join-Path $currentwd "dist")
+$bundle_path = (Join-Path $distpath "rvclient-win32")
+$temp_bundle = (Join-Path $distpath "temp.zip")
+
+
+$bundle_final_path = (Join-Path $currentwd "\bundles\rvclient-win32-$suffix.zip")
+$exe_final_path = (Join-Path $currentwd "\bundles\rvclient-win32-standalone-$suffix.exe")
+
+
+if ( (Test-Path "bundles") -eq $False)
+{
+	echo "Creating bundles directory"
+	New-Item -ItemType directory -Path (Join-Path $currentwd "bundles")
+}
+
+if (Test-Path "$temp_bundle")
+{
+	Delete-Item "$temp_bundle"
+}
+
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$bundle_path", "$temp_bundle", $compressionLevel, $false)
+Move-Item -force "$temp_bundle" "$bundle_final_path"
+
+
+Copy-Item -force dist\rvclient-win32-standalone.exe "$exe_final_path"
